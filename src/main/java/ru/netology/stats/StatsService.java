9cab2ab7e29e26f38d02f3597c831f1efda570f4
Siya <aianchilenko@gmail.com>
2025-10-26 09:38:42 +0300

    Updated StatsService.java and StatsServiceTest.java, modified .gitignore and pom.xml

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,60 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    // Метод для вычисления общей суммы продаж за год (по всем месяцам)
+    public long totalSales(long[] sales) {
+        long total = 0;
+        for (long sale : sales) {
+            total += sale;
+        }
+        return total;
+    }
+
+    public long averageSales(long[] sales) {
+        return totalSales(sales) / 12;
+
+    }
+
+    public int monthMaximum(long[] sales) {
+        int month = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[month]) {
+                month = i;
+            }
+        }
+        return month + 1;
+    }
+
+    public int monthMinimum(long[] sales) {
+        int month = 0;
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] < sales[month]) {
+                month = i;
+            }
+        }
+        return month + 1;
+    }
+
+    public int monthesBellowAverageMin(long[] sales) {
+        long average = averageSales(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public int monthesAboveAverageMax(long[] sales) {
+        long average = averageSales(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,63 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class StatsServiceTest {
+    @Test
+    public void testTotalSale() {
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};  // массив с продажами
+        StatsService statsService = new StatsService();  // создаем объект StatsService
+
+        long actual = statsService.totalSales(sale);  // вызываем метод totalSale
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;  // сумма всех продаж
+        Assertions.assertEquals(expected, actual);  // проверка, что результат совпадает
+    }
+
+    @Test
+    public void testAverageSale() {
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService statsService = new StatsService();
+
+        long actual = statsService.averageSales(sale);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testManthMaximumSale() {
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService statsService = new StatsService();
+
+        long actual = statsService.monthMaximum(sale);
+        long expected = 8;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testMonthMinimumSale() {
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService statsService = new StatsService();
+
+        long actual = statsService.monthMinimum(sale);
+        long expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testMonthBellowAverageMin() {
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService statsService = new StatsService();
+
+        long actual = statsService.monthesBellowAverageMin(sale);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testMonthesAboveAverageMax() {
+        long[] sale = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService statsService = new StatsService();
+
+        long actual = statsService.monthesAboveAverageMax(sale);
+        long expected = 6;  // количество месяцев, где продажи выше среднего
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
